ee7eb463dedce0cdf5af4bce362323020ca0475a
Budhi Octaviansyah <budioct@gmail.com>
2024-11-28 14:50:18 +0700

    add post entity

---
+++ src/main/java/budhioct/dev/entity/PostEntity.java
@@ -0,0 +1,36 @@
+package budhioct.dev.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners({AuditingEntityListener.class})
+@Entity
+@Table(name = "posts")
+public class PostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "title")
+    private String title;
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(name = "updated_at", insertable = false)
+    private LocalDateTime updatedAt;
+
+}
