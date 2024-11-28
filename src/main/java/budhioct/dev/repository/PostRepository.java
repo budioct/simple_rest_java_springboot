61d2ae89ab456f139edffcc2c480413441a8aedc
Budhi Octaviansyah <budioct@gmail.com>
2024-11-28 14:50:45 +0700

    add post repository

---
+++ src/main/java/budhioct/dev/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package budhioct.dev.repository;
+
+import budhioct.dev.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface PostRepository extends JpaRepository<PostEntity, Long>, JpaSpecificationExecutor<PostEntity> {
+}
