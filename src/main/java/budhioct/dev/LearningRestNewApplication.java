8c1bf979cb6167d785c0ccab617dee5d7d111d24
Budhi Octaviansyah <budioct@gmail.com>
2024-11-28 12:56:58 +0700

    start project

---
+++ src/main/java/budhioct/dev/LearningRestNewApplication.java
@@ -0,0 +1,13 @@
+package budhioct.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRestNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningRestNewApplication.class, args);
+	}
+
+}
