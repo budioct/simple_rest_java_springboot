21136a492e70a91bbd9750fd6c157e25881f329e
Budhi Octaviansyah <budioct@gmail.com>
2024-11-29 15:47:15 +0700

    add fitur rest service post soft delete

--- src/main/java/budhioct/dev/entity/PostEntity.java
+++ src/main/java/budhioct/dev/entity/PostEntity.java
@@ -2,10 +2,12 @@ package budhioct.dev.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,6 +21,8 @@ import java.time.LocalDateTime;
 @EntityListeners({AuditingEntityListener.class})
 @Entity
 @Table(name = "posts")
+@SQLDelete(sql = "update posts set delete_at = true    where id = ?")
+@Where(clause = "delete_at = false")
 public class PostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,5 +36,7 @@ public class PostEntity {
     @LastModifiedDate
     @Column(name = "updated_at", insertable = false)
     private LocalDateTime updatedAt;
+    @Column(name = "delete_at")
+    private boolean deleteAt = Boolean.FALSE;
 
 }
